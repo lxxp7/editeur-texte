4f7d78e17a092e9df636ed97fe1e3fc81e01839f
Schott Liam <liam.schott@etu.umontpellier.fr>
2023-11-07 16:43:23 +0100

    ajout commande + rebase

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;start;end;texte");
+            return;
+        }
+
+        String texte = parameters[3];
+        int start = Integer.parseInt(parameters[1]);
+        int end = Integer.parseInt(parameters[2]);
+        this.document.remplacer(start,end,texte);
+        super.executer();
+    }
+
+}
