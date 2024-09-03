d34245ed88d5d92de614a2e3526bd6969f348c47
Schott Liam <liam.schott@etu.umontpellier.fr>
2024-09-03 18:06:40 +0200

    ajout majuscule marche

---
+++ src/main/java/fr/iut/editeur/commande/CommandeMajuscule.java
@@ -0,0 +1,24 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeMajuscule extends CommandeDocument{
+
+    public CommandeMajuscule(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : majuscule;start;end");
+            return;
+        }
+        int start = Integer.parseInt(parameters[1]);
+        int end = Integer.parseInt(parameters[2]);
+        this.document.majuscules(start,end);
+        super.executer();
+    }
+
+}
+
--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -26,6 +26,12 @@ public class Document {
         texte = partieGauche + remplacement + partieDroite;
     }
 
+    public void majuscules(int debut, int fin) {
+        String partieGauche = texte.substring(debut,fin + 1);
+        remplacer(debut,fin,partieGauche.toUpperCase());
+    }
+
+
     @Override
     public String toString() {
         return this.texte;
