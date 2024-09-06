ea3c4b807fd72cf2a665361d0822d504b75e3612
BELHAJ Yaniss <yaniss.belhaj@etu.umontpellier.fr>
2024-09-06 07:57:04 +0100

    ajout class CommandeMinuscule

---
+++ src/main/java/fr/iut/editeur/commande/CommandeMinuscule.java
@@ -0,0 +1,22 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeMinuscule extends CommandeDocument{
+
+    public CommandeMinuscule(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 3) {
+            System.err.println("Format attendu : minuscules;start;end");
+            return;
+        }
+        int start = Integer.parseInt(parameters[1]);
+        int end = Integer.parseInt(parameters[2]);
+        this.document.minuscules(start,end);
+        super.executer();
+    }
+}
